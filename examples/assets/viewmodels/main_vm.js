8a88f4ed629bae90f1d325fbce3ae478c37bf92b
mikegai <mike.d.gai@gmail.com>
2012-07-18 17:12:47 -0700

    Made focusProperty explicit in assets example for clarity

--- examples/assets/viewmodels/main_vm.js
+++ examples/assets/viewmodels/main_vm.js
@@ -6,7 +6,8 @@ glu.defModel('examples.assets.main', {
     sliceCounter:1,
 
     assetSetList:{
-        mtype:'activatorlist'
+        mtype:'activatorlist',
+        focusProperty:'activeSetWithFocus'
     },
 
     assetSetWithFocus: {mtype:'assetSet', name:'dummy'},
