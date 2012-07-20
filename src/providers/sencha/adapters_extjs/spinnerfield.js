6785024dffa61262b68bb023b0efb615290ce131
mikegai <mike.d.gai@gmail.com>
2012-07-19 17:13:09 -0700

    Fixed problem with binding not working on numberfield (spinnerfield)

---
+++ src/providers/sencha/adapters_extjs/spinnerfield.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2012 by CoNarrative
+ */
+/**
+ * @class glu.extjs.adapters.spinnerfield
+ * @author Mike Gai
+ * @extends glu.extjs.adapters.field
+ *
+ * A binder that adapts spinners (like the Number field)
+ */
+glu.regAdapter('spinnerfield', {
+    extend :'field',
+    beforeCreate:function (config) {
+        config.enableKeyEvents = true;
+    },
+    afterCreate:function (control, viewmodel) {
+        if (glu.testMode) {
+            control.addListener('keyup', function () {
+                control.fireEvent('valuechanged', control);
+            }, control);
+            return;
+        }
+        //adds a buffer to all key events
+        if (!control.delayedEvent) {
+            control.delayedEvent = new Ext.util.DelayedTask(function () {
+                control.fireEvent('valuechanged', control);
+            });
+        }
+        control.addListener('keyup', function () {
+            control.delayedEvent.delay(control.keyDelay || 100); //give some time for multiple keypresses...
+        }, control);
+        control.addListener('spin', function(){
+            control.delayedEvent.delay(control.keyDelay || 100);
+        })
+    },
+    valueBindings:{
+        eventName:'valuechanged',
+        eventConverter:function (control) {
+            return control.getValue();
+        }
+    }
+});
+
