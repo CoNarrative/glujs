1fbb448a33bdf42a082c4da086fe2cc6ee38c6d3
mikegai <mike.d.gai@gmail.com>
2012-08-06 13:00:03 -0700

    Added some additional logic to assets example

--- examples/assets/models/asset_model.js
+++ examples/assets/models/asset_model.js
@@ -19,35 +19,17 @@ glu.namespace('examples.assets.models').asset = {
         }, {
             name:'yearsOfMaintenance',
             type:'number'
+        }, {
+            name:'maintenanceStartDate',
+            type:'date'
         }
     ]
 };
-glu.defRowModel('examples.assets.models.Asset',{
-    fields:[
-        {
-            name:'id',
-            type:'int'
-        },
-        {
-            name:'name',
-            type:'string'
-        },
-        {
-            name:'status',
-            type:'string',
-            oneOf : ['active','storage','verifying']
-        },
-        {
-            name:'lastVerified',
-            type:'date'
-        }, {
-            name:'yearsOfMaintenance',
-            type:'number'
-        }
-    ] ,
+var rowModel = glu.deepApply({
     formulas:{
         yearsMatter: function(){
             return this.status=='active';
         }
     }
-});
+},examples.assets.models.asset );
+glu.defRowModel('examples.assets.models.Asset',rowModel);
