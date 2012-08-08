66ec14034472e58885d460987ebac8efba2b5965
smithrp <smithrp@gmail.com>
2012-08-08 15:03:26 -0700

    Added parentVM to open method for viewmodel to correctly parent the opened viewmodel

--- src/viewmodel.js
+++ src/viewmodel.js
@@ -631,7 +631,7 @@ glu.Viewmodel = glu.extend(Object, {
      */
     open:function (config) {
         config.ns = config.ns || this.ns;
-		config.parentVM = config.parentVM || this.parentVM;
+		config.parentVM = config.parentVM || this;
         var win = glu.openWindow(config);
         return win._bindings.viewmodel;
     },
