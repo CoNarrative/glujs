dcd192697b11a103b90f66856f874b1cacb6ad94
mikegai <mike.d.gai@gmail.com>
2013-01-31 18:26:01 -0800

    Made generated ids much more readable and clearly marked as bound by glu

--- src/providers/sencha/binder_collect.js
+++ src/providers/sencha/binder_collect.js
@@ -312,7 +312,7 @@ Ext.apply(glu.provider.binder, {
 
         //STEP 9: Store the binding in the list and return
         if (config._bindings != null && config._bindings.length > 0) {
-            config.id = config.id || Ext.id();
+            config.id = config.id || Ext.id(null,'glu-' + config.xtype + '-');
             config._bindings.defaultModel = viewmodel;
             config._bindings.adapter = xtypeAdapter;
             bindingsList.push(config);
