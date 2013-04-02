eefc3a64eade63f202ad928ae4c423d956052ece
smithrp <smithrp@gmail.com>
2013-04-01 20:15:45 -0700

    commented out tpl bindings because they break dataview using tpl

--- src/providers/sencha/adapters_extjs/component.js
+++ src/providers/sencha/adapters_extjs/component.js
@@ -180,18 +180,18 @@ glu.regAdapter('component', {
                 });
             });
         }
-        if (control.tpl){
-            //VERY SPECIAL BINDINGS!
-            var task = new Ext.util.DelayedTask(
-                function(){
-                    control.update(viewmodel);
-                    control.fireEvent('updated',control);
-                });
-            control.on('render',function(){control.fireEvent('updated',control)});
-            control.data = viewmodel; //use viewmodel as initialtemplate source
-            //TODO: FInd all the bound guys and LISTEN ON THEM!
-            viewmodel.on('bulkupdatecommitted', function(){task.delay(10);});
-        }
+        // if (control.tpl){
+        //     //VERY SPECIAL BINDINGS!
+        //     var task = new Ext.util.DelayedTask(
+        //         function(){
+        //             control.update(viewmodel);
+        //             control.fireEvent('updated',control);
+        //         });
+        //     control.on('render',function(){control.fireEvent('updated',control)});
+        //     control.data = viewmodel; //use viewmodel as initialtemplate source
+        //     //TODO: FInd all the bound guys and LISTEN ON THEM!
+        //     viewmodel.on('bulkupdatecommitted', function(){task.delay(10);});
+        // }
     }
 
     /**
