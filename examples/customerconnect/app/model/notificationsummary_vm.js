ecd9ce1712bd4d4e05cb4f9adf4fdeb42bd520ed
Travis Barajas <tvbarajas@gmail.com>
2013-07-02 15:46:31 -0700

    Notification Summary is showing data via the loadData method of the Model.

--- examples/customerconnect/app/model/notificationsummary_vm.js
+++ examples/customerconnect/app/model/notificationsummary_vm.js
@@ -6,30 +6,22 @@ glu.defModel('ps.notificationSummary', {
     type: '',
     updateActiveScreen: function () {
 
-        this.parentVM.parentVM.set('selectedNotificationSummary',this);
+        this.parentVM.parentVM.set('selectedNotificationSummary', this);
         this.parentVM.parentVM.updateActiveScreen();
     }
 })
 glu.defModel('ps.notificationSummaryDetail', {
-//    config:{
-//        fields:['id','message','count','type']
-//    },
-    fields:['id','message','count','type'],
+    fields: ['id', 'message', 'count', 'type'],
     id: '',
     message: '',
     count: '',
     type: '',
-    when_selectedNotificationSummary_changes:{
-        on:['parentVM.selectedNotificationSummaryChanged'],
-        action:function(){
-            debugger;
-          //  this.loadData(this.parentVM.selectedNotificationSummary);
+    when_selectedNotificationSummary_changes: {
+        on: ['parentVM.selectedNotificationSummaryChanged'],
+        action: function () {
+            this.loadData(this.parentVM.selectedNotificationSummary);
         }
     }
-//    summary$: function () {
-//        debugger;
-//        return this.parentVM.selectedNotificationSummary;
-//    }
 })
 
 
